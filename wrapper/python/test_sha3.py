d5a071097bec2a993792312d6205bcbda9501bc0
Anjan Roy <hello@itzmeanjan.in>
2022-09-15 12:13:31 +0400

    test functional correctness of sha3-512 implementation, using Python interface & Python `hashlib` module
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- wrapper/python/test_sha3.py
+++ wrapper/python/test_sha3.py
@@ -57,5 +57,20 @@ def test_sha3_384_hash():
         ), f"[SHA3-384] Expected {dig1.hex()}, found {dig0.hex()}, for input {msg.hex()}"
 
 
+def test_sha3_512_hash():
+    """
+    Test functional correctness of SHA3-512 hash function implementation
+    """
+    for i in range(FROM_BYTES, TO_BYTES + 1):
+        msg = gen_rand_bytes(i)
+
+        dig0 = sha3.sha3_512_hash(msg)
+        dig1 = hashlib.sha3_512(msg).digest()
+
+        assert (
+            dig0 == dig1
+        ), f"[SHA3-512] Expected {dig1.hex()}, found {dig0.hex()}, for input {msg.hex()}"
+
+
 if __name__ == "__main__":
     print("Use `pytest` for executing test cases")
