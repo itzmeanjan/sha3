b547ed345dccb2997dc2c44ca268c448f62dab26
Anjan Roy <hello@itzmeanjan.in>
2022-09-15 12:16:01 +0400

    benchmark sha3-512 hash function on CPU system
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -34,5 +34,15 @@ BENCHMARK(bench_sha3::sha3_384)->Arg(1024);
 BENCHMARK(bench_sha3::sha3_384)->Arg(2048);
 BENCHMARK(bench_sha3::sha3_384)->Arg(4096);
 
+// register sha3-512 hash function for benchmark
+BENCHMARK(bench_sha3::sha3_512)->Arg(32);
+BENCHMARK(bench_sha3::sha3_512)->Arg(64);
+BENCHMARK(bench_sha3::sha3_512)->Arg(128);
+BENCHMARK(bench_sha3::sha3_512)->Arg(256);
+BENCHMARK(bench_sha3::sha3_512)->Arg(512);
+BENCHMARK(bench_sha3::sha3_512)->Arg(1024);
+BENCHMARK(bench_sha3::sha3_512)->Arg(2048);
+BENCHMARK(bench_sha3::sha3_512)->Arg(4096);
+
 // benchmark runner main routine
 BENCHMARK_MAIN();
