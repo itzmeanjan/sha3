b20ca7f288612660f4f5f2583fdb3624cb964ba3
Anjan Roy <hello@itzmeanjan.in>
2022-12-04 12:23:37 +0400

    on x86_64 architecture use compiler built-in `__rdtsc` for computing CPU cycle count
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/bench/bench_keccak.hpp
+++ include/bench/bench_keccak.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "keccak.hpp"
 #include "utils.hpp"
+#include "x86_64_cpu_cycles.hpp"
 #include <benchmark/benchmark.h>
 
 // Benchmarks SHA3 functions on CPU systems, using google-benchmark
@@ -15,8 +16,21 @@ keccakf1600(benchmark::State& state)
   constexpr size_t len = sizeof(st) / sizeof(st[0]);
   random_data<uint64_t>(st, len);
 
+#if defined __x86_64__
+  uint64_t total_cycles = 0ul;
+#endif
+
   for (auto _ : state) {
+#if defined __x86_64__
+    const uint64_t start = cpu_cycles();
+#endif
+
     keccak::permute(st);
+
+#if defined __x86_64__
+    const uint64_t end = cpu_cycles();
+    total_cycles += (end - start);
+#endif
 
     benchmark::DoNotOptimize(st);
     benchmark::ClobberMemory();
@@ -24,6 +38,11 @@ keccakf1600(benchmark::State& state)
 
   const size_t tot_bytes = state.iterations() * sizeof(st);
   state.SetBytesProcessed(static_cast<int64_t>(tot_bytes));
+
+#if defined __x86_64__
+  total_cycles /= static_cast<uint64_t>(state.iterations());
+  state.counters["average_cpu_cycles"] = static_cast<double>(total_cycles);
+#endif
 }
 
 }
