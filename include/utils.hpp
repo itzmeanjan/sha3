976960a2c7439d636f14ba94af3ca4cf7f76bf0b
Anjan Roy <hello@itzmeanjan.in>
2023-04-01 12:03:09 +0400

    interpret byte array as array of little-endian words
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -35,3 +35,31 @@ to_hex(const uint8_t* const bytes, const size_t len)
 
   return ss.str();
 }
+
+// Given a byte array holding rate/8 -many bytes, this routine can be invoked
+// for interpreting those bytes as rate/ 64 -many words ( each word is 64 -bit
+// unsigned interger ) s.t. bytes in a word are placed in little-endian order.
+template<const size_t rate>
+static inline void
+bytes_to_le_words(const uint8_t* const __restrict bytes,
+                  uint64_t* const __restrict words)
+{
+  constexpr size_t rbytes = rate >> 3;   // # -of bytes
+  constexpr size_t rwords = rbytes >> 3; // # -of 64 -bit words
+
+  if constexpr (std::endian::native == std::endian::little) {
+    std::memcpy(words, bytes, rbytes);
+  } else {
+    for (size_t j = 0; j < rwords; j++) {
+      const size_t boff = j << 3;
+      words[j] = (static_cast<uint64_t>(bytes[boff + 7]) << 56) |
+                 (static_cast<uint64_t>(bytes[boff + 6]) << 48) |
+                 (static_cast<uint64_t>(bytes[boff + 5]) << 40) |
+                 (static_cast<uint64_t>(bytes[boff + 4]) << 32) |
+                 (static_cast<uint64_t>(bytes[boff + 3]) << 24) |
+                 (static_cast<uint64_t>(bytes[boff + 2]) << 16) |
+                 (static_cast<uint64_t>(bytes[boff + 1]) << 8) |
+                 (static_cast<uint64_t>(bytes[boff + 0]) << 0);
+    }
+  }
+}
