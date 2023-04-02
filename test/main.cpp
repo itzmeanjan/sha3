7b91c9080cbe72efae78d21e8eabbfd0c1fa4ff1
Anjan Roy <hello@itzmeanjan.in>
2023-04-02 13:17:43 +0400

    test functional correctness of shake{128, 256} xof absorption API
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -5,28 +5,14 @@
 int
 main()
 {
-  constexpr size_t shake128_rate = shake128::rate >> 3;
-  constexpr size_t shake256_rate = shake256::rate >> 3;
+  constexpr size_t MIN_MSG_LEN = 0;
+  constexpr size_t MAX_MSG_LEN = 1025;
 
-  test_sha3::test_shake128<0, shake128_rate>();
-  test_sha3::test_shake128<1, shake128_rate>();
-  test_sha3::test_shake128<1, shake128_rate - 1>();
-  test_sha3::test_shake128<2, shake128_rate - 2>();
-  test_sha3::test_shake128<2, shake128_rate - 1>();
-  test_sha3::test_shake128<shake128_rate - 1, 1>();
-  test_sha3::test_shake128<shake128_rate - 2, 2>();
-  test_sha3::test_shake128<shake128_rate - 1, 2>();
-  std::cout << "[test] SHAKE128 incremental hashing\n";
-
-  test_sha3::test_shake256<0, shake256_rate>();
-  test_sha3::test_shake256<1, shake256_rate>();
-  test_sha3::test_shake256<1, shake256_rate - 1>();
-  test_sha3::test_shake256<2, shake256_rate - 2>();
-  test_sha3::test_shake256<2, shake256_rate - 1>();
-  test_sha3::test_shake256<shake256_rate - 1, 1>();
-  test_sha3::test_shake256<shake256_rate - 2, 2>();
-  test_sha3::test_shake256<shake256_rate - 1, 2>();
-  std::cout << "[test] SHAKE256 incremental hashing\n";
+  for (size_t i = MIN_MSG_LEN; i < MAX_MSG_LEN; i++) {
+    test_sha3::test_shake128(i);
+    test_sha3::test_shake256(i);
+  }
+  std::cout << "[test] SHAKE{128, 256} incremental message absorption\n";
 
   return EXIT_SUCCESS;
 }
