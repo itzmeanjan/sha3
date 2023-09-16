f30dd8a90a69ea4bc2072fc6c607e44d7c467198
Anjan Roy <hello@itzmeanjan.in>
2023-09-16 12:28:24 +0400

    relocate benchmarks to different directory
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benchmarks/bench_keccak.cpp
+++ benchmarks/bench_keccak.cpp
@@ -1,14 +1,10 @@
-#pragma once
 #include "keccak.hpp"
 #include "utils.hpp"
 #include <benchmark/benchmark.h>
 
-// Benchmarks SHA3 functions targeting CPU systems, using google-benchmark.
-namespace bench_sha3 {
-
 // Benchmarks Keccak-p[1600, 24] permutation.
-inline void
-keccakf1600(benchmark::State& state)
+void
+bench_keccak_permutation(benchmark::State& state)
 {
   uint64_t st[keccak::LANE_CNT]{};
   sha3_utils::random_data<uint64_t>(st);
@@ -33,4 +29,4 @@ keccakf1600(benchmark::State& state)
 #endif
 }
 
-}
+BENCHMARK(bench_keccak_permutation);
