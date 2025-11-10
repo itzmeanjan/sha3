9c619b05bd0fce60be23e09733f9ac6bcb3d6215
Anjan Roy <hello@itzmeanjan.in>
2025-11-10 20:36:25 +0530

    Add support for benchmarking TurboSHAKE XOFs
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benches/bench_xof.cpp
+++ benches/bench_xof.cpp
@@ -1,12 +1,16 @@
 #include "bench_common.hpp"
 #include "sha3/shake128.hpp"
 #include "sha3/shake256.hpp"
+#include "sha3/turboshake128.hpp"
+#include "sha3/turboshake256.hpp"
 #include <benchmark/benchmark.h>
 
-// Benchmarks SHAKE-128 extendable output function with variable length input and squeezed output.
-//
-// Note, all input bytes are absorbed in a single call to `absorb` function.
-// And all output bytes are squeezed in a single call to `squeeze` function.
+/**
+ * Benchmarks SHAKE128 extendable output function with variable length input and squeezed output.
+ *
+ * Note, all input bytes are absorbed in a single call to `absorb` function.
+ * And all output bytes are squeezed in a single call to `squeeze` function.
+ */
 void
 bench_shake128(benchmark::State& state)
 {
@@ -38,10 +42,12 @@ bench_shake128(benchmark::State& state)
 #endif
 }
 
-// Benchmarks SHAKE-256 extendable output function with variable length input and squeezed output.
-//
-// Note, all input bytes are absorbed in a single call to `absorb` function.
-// And all output bytes are squeezed in a single call to `squeeze` function.
+/**
+ * Benchmarks SHAKE256 extendable output function with variable length input and squeezed output.
+ *
+ * Note, all input bytes are absorbed in a single call to `absorb` function.
+ * And all output bytes are squeezed in a single call to `squeeze` function.
+ */
 void
 bench_shake256(benchmark::State& state)
 {
@@ -73,6 +79,80 @@ bench_shake256(benchmark::State& state)
 #endif
 }
 
+/**
+ * Benchmarks TurboSHAKE128 extendable output function with variable length input and squeezed output.
+ *
+ * Note, all input bytes are absorbed in a single call to `absorb` function.
+ * And all output bytes are squeezed in a single call to `squeeze` function.
+ */
+void
+bench_turboshake128(benchmark::State& state)
+{
+  const size_t mlen = static_cast<size_t>(state.range(0));
+  const size_t olen = static_cast<size_t>(state.range(1));
+
+  std::vector<uint8_t> msg(mlen);
+  std::vector<uint8_t> out(olen);
+
+  random_data<uint8_t>(msg);
+
+  for (auto _ : state) {
+    turboshake128::turboshake128_t hasher;
+    hasher.absorb(msg);
+    hasher.finalize();
+    hasher.squeeze(out);
+
+    benchmark::DoNotOptimize(hasher);
+    benchmark::DoNotOptimize(msg);
+    benchmark::DoNotOptimize(out);
+    benchmark::ClobberMemory();
+  }
+
+  const size_t bytes_processed = state.iterations() * (msg.size() + out.size());
+  state.SetBytesProcessed(bytes_processed);
+
+#ifdef CYCLES_PER_BYTE
+  state.counters["CYCLES/ BYTE"] = state.counters["CYCLES"] / bytes_processed;
+#endif
+}
+
+/**
+ * Benchmarks TurboSHAKE256 extendable output function with variable length input and squeezed output.
+ *
+ * Note, all input bytes are absorbed in a single call to `absorb` function.
+ * And all output bytes are squeezed in a single call to `squeeze` function.
+ */
+void
+bench_turboshake256(benchmark::State& state)
+{
+  const size_t mlen = static_cast<size_t>(state.range(0));
+  const size_t olen = static_cast<size_t>(state.range(1));
+
+  std::vector<uint8_t> msg(mlen);
+  std::vector<uint8_t> out(olen);
+
+  random_data<uint8_t>(msg);
+
+  for (auto _ : state) {
+    turboshake256::turboshake256_t hasher;
+    hasher.absorb(msg);
+    hasher.finalize();
+    hasher.squeeze(out);
+
+    benchmark::DoNotOptimize(hasher);
+    benchmark::DoNotOptimize(msg);
+    benchmark::DoNotOptimize(out);
+    benchmark::ClobberMemory();
+  }
+
+  const size_t bytes_processed = state.iterations() * (msg.size() + out.size());
+  state.SetBytesProcessed(bytes_processed);
+
+#ifdef CYCLES_PER_BYTE
+  state.counters["CYCLES/ BYTE"] = state.counters["CYCLES"] / bytes_processed;
+#endif
+}
+
 BENCHMARK(bench_shake128)
   ->ArgsProduct({ benchmark::CreateRange(64, 16384, 4), { 64 } })
   ->Name("shake128")
@@ -81,5 +161,15 @@ BENCHMARK(bench_shake128)
 BENCHMARK(bench_shake256)
   ->ArgsProduct({ benchmark::CreateRange(64, 16384, 4), { 64 } })
   ->Name("shake256")
+  ->ComputeStatistics("min", compute_min)
+  ->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_turboshake128)
+  ->ArgsProduct({ benchmark::CreateRange(64, 16384, 4), { 64 } })
+  ->Name("turboshake128")
+  ->ComputeStatistics("min", compute_min)
+  ->ComputeStatistics("max", compute_max);
+BENCHMARK(bench_turboshake256)
+  ->ArgsProduct({ benchmark::CreateRange(64, 16384, 4), { 64 } })
+  ->Name("turboshake256")
   ->ComputeStatistics("min", compute_min)
   ->ComputeStatistics("max", compute_max);
